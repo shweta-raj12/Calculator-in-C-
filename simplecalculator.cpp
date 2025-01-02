8d438023393c7fe1300a1f17db8182c74de92514
Shweta Raj <shwetaraj532@gmail.com>
2025-01-02 13:20:33 +0530

    Add files via upload

---
+++ simplecalculator.cpp
@@ -0,0 +1,41 @@
+
+
+//Simple Calculator in C++ by using Switch Case
+//user input number and choose Operator
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a, b;
+    char c;
+    cout << "Enter the value of A " << endl;
+    cin >> a;
+    cout << "Enter the value of B" << endl;
+    cin >> b;
+    cout << "Enter any Operator : +,-,*,/ " << endl;
+    cin >> c;
+
+    switch (c)
+    {
+    case '+':
+        cout << a<<"+"<<b<<"="<<a+b << endl;
+        break ;
+
+    case '-':
+        cout << a<<"-"<<b<<"="<<a-b << endl;
+        break ;
+
+    case '*':
+        cout << a<<"*"<<b<<"="<<a*b << endl;
+        break ;
+
+    case '/':
+        cout << a<<"/"<<b<<"="<<a/b << endl;
+        break ;
+    default :
+        cout << "Error Value " << endl;
+    }
+    return 0;
+}
